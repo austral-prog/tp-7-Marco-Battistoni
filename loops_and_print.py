3fb3819818536a5a513ec52ee0c66bb2d33cd1e5
Marco-Battistoni <162721472+Marco-Battistoni@users.noreply.github.com>
2024-05-18 16:46:13 -0300

    Add files via upload

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,15 @@
 def enumerate_list(list):
-    return list
+    list.remove("")
+    new_list=[]
+    for index, color in enumerate(list):
+        new_list.append(f"{index}. {color}")
+    return new_list
 
 
 def enumerate_backwards(list):
-    return list
+    list.remove("")
+    new_list=[]
+    for index, color in enumerate(list):
+        color = color[::-1]
+        new_list.append(f"{index}. {color}")
+    return new_list
